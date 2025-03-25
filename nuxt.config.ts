ac05b9025c100924d374b563bf6cdfdc2d979e84
RedStars071 <redtwoghost@gmail.com>
2025-03-25 16:18:43 +0100

    refactor: update nuxt.config.ts for shadcn component configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,13 @@
-import tailwindcss from '@tailwindcss/vite'
-import 'nuxt'
+import tailwindcss from '@tailwindcss/vite';
+import 'nuxt';
 
 export default defineNuxtConfig({
-  modules: [
-    'reka-ui',
-    '@nuxtjs/color-mode',
-    '@nuxt/icon',
-    'shadcn-nuxt',
-    '@nuxt/eslint',
-  ],
-  css: ['assets/css/main.css'],
-  compatibilityDate: '2025-02-19',
-  vite: { plugins: [tailwindcss()] },
-})
+	modules: ['reka-ui', '@nuxtjs/color-mode', '@nuxt/icon', 'shadcn-nuxt', '@nuxt/eslint'],
+	shadcn: {
+		prefix: 'U',
+		componentDir: './components/ui'
+	},
+	css: ['assets/css/main.css'],
+	compatibilityDate: '2025-02-19',
+	vite: { plugins: [tailwindcss()] }
+});
