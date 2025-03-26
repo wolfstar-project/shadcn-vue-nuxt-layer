3700c5c96f6db5f5f665e823fbd8358fb8abfcad
RedStars071 <redtwoghost@gmail.com>
2025-03-26 15:37:25 +0100

    chore: add .npmrc for dependency management and update nuxt.config.ts module configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import 'nuxt';
 
 export default defineNuxtConfig({
-	modules: ['reka-ui', '@nuxtjs/color-mode', '@nuxt/icon', 'shadcn-nuxt', '@nuxt/eslint'],
-	shadcn: {
-		prefix: 'U',
-		componentDir: './components/ui'
-	},
+	modules: ['reka-ui/nuxt', '@nuxtjs/color-mode', '@nuxt/icon', 'shadcn-nuxt', '@nuxt/eslint'],
 	css: ['assets/css/main.css'],
 	compatibilityDate: '2025-02-19',
 	vite: { plugins: [tailwindcss()] }
